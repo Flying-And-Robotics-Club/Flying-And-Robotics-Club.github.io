fa28187e706f94fa3717aff359b57fad94cd16f5
bigBrain1901 <asishaan.191cs124@nitk.edu.in>
2020-03-13 02:25:19 +0530

    Test push

---
+++ js/winter-projects.js
@@ -0,0 +1,34 @@
+function buttonClick (index){
+    switch (index) {
+        case 1 :
+            setTimeout(function(){
+                $("#data1").slideToggle(300, "swing", function(){/*done*/});
+            }, 700);
+            break;
+        case 2 :
+            setTimeout(function(){
+                $("#data2").slideToggle(300, "swing", function(){/*done*/});
+            }, 700);
+            break;
+        case 3 :
+            setTimeout(function(){
+                $("#data3").slideToggle(300, "swing", function(){/*done*/});
+            }, 700);
+            break;
+        case 4 :
+            setTimeout(function(){
+                $("#data4").slideToggle(300, "swing", function(){/*done*/});
+            }, 700);
+            break;
+        case 5 :
+            setTimeout(function(){
+                $("#data5").slideToggle(300, "swing", function(){/*done*/});
+            }, 700);
+            break;
+        case 6 :
+            setTimeout(function(){
+                $("#data6").slideToggle(300, "swing", function(){/*done*/});
+            }, 700);
+            break;
+    }
+}
