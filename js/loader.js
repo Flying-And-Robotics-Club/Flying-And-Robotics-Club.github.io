753e7532e8f04685bb5bde4f0ecfbdd827afba4d
Ishaan Singh <asishaan.191cs124@nitk.edu.in>
2020-07-07 07:38:01 +0530

    Updated gallery loader

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,9 @@
 $(document).on("load", function() {
-    console.log ("loading");
     $("#loader").show();
 });
 
 $(document).ready(function() {
-    console.log ("ready"); $("#loader").show();
     setTimeout(function() {
-        console.log ("faded out");
         $("#loader").fadeOut("slow");
-    }, 1000);
+    }, $("#loader").attr("loader-time") || 1000);
 });
